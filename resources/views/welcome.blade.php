610e1a67a80566fd04e577a44dbdeb4350c3ad2f
Rochmad Nurul Fahmi <53666901+rochmadnf@users.noreply.github.com>
2023-12-23 16:06:54 +0800

    captcha show

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,14 @@
 
 <body>
     <h1>Hai, Call me Alex.</h1>
+
+    <p>Please Fill Captcha code below:</p>
+
+    <div
+        style="display: flex; flex-direction: column; gap: 1rem; border: solid 1px black; padding: 1rem; border-radius: 0.75rem;">
+        <img src="{{ $captchaPath }}" alt="captcha code">
+        <input placeholder="write code here..." />
+    </div>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Gregwar\Captcha\CaptchaBuilder;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $builder = new CaptchaBuilder();
+
+    $builder->build();
+
+    $captchaPath = $builder->inline();
+
+    return view('welcome', compact('captchaPath'));
 });
